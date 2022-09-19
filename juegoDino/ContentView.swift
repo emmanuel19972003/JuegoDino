82521025cc1037894076fbf81b99f86da773703b
emmanuel Z <emmanuel1997@gmailcom>
2022-09-19 16:02:22 -0500

    Initial Commit

---
+++ juegoDino/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  juegoDino
+//
+//  Created by Emmanuel Zambrano Quitian on 9/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ juegoDino/juegoDinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  juegoDinoApp.swift
+//  juegoDino
+//
+//  Created by Emmanuel Zambrano Quitian on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct juegoDinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
